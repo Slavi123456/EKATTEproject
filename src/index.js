e5293147db2955566ec73f2910f88af9d3235e39
Slavi123456 <74818665+Slavi123456@users.noreply.github.com>
2025-11-27 13:49:44 +0200

    First steps of a server

--- src/index.js
+++ src/index.js
@@ -1,59 +1,68 @@
-import pg from 'pg';
-import district__json_file from './data/ek_obl.json' with {type: 'json'};
-import township__json_file from './data/ek_obst.json' with {type: 'json'};
-import cityhalls__json_file from './data/ek_kmet.json' with {type: 'json'};
-import villages__json_file from './data/ek_sobr.json' with {type: 'json'};
-import dotenv from 'dotenv';
+import http from "http";
+import dotenv from "dotenv";
+import client from "./config/db.js";
+import { bulk_inserts_from_json } from "./services/databese_inserts.js";
 
-// import { insert_into_district, insert_into_township, 
-//         insert_into_cityhalls, insert_into_villages } from "./services/databese_inserts.js";
-import { safe_insert_into_table } from './services/databese_inserts.js';
-import { safe_get_village_values } from './services/village.js';
-
-"use strict";
+("use strict");
 
 dotenv.config();
 
-const { Client } = pg;
+const hostname = process.env.HOST;
+const port = process.env.PORT;
 
-const client = new Client({
-    user: process.env.PDB_USER,
-    password: process.env.PDB_PASSWORD,
-    host: process.env.PDB_HOST,
-    port: process.env.PDB_PORT,
-    database: process.env.PDB_NAME,
-});
-await client.connect();
+main();
 
+function main() {
+  const server = http.createServer(async (req, res) => {
+    await routing_dispatcher(req, res);
+  });
 
-try {
+  server.on("clientError", (err, socket) => {
+    socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
+  });
 
-    //CHECK FOR CONNECTION
-    // const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-    // console.log(res.rows[0].message)
-
-    // const curr_db = await client.query('SELECT current_database()');
-    // console.log(curr_db.rows);
-
-    ///INSERT
-    const district_insert = 'INSERT INTO district(id, name, name_en, center_id) VALUES($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING *';
-    await safe_insert_into_table(district_insert, client, 'District', district__json_file, row => [row.oblast, row.name, row.name_en, row.ekatte]);
-
-    const township_insert = ' INSERT INTO township(id, name, name_en, district_id, center_id) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING RETURNING *;';
-    await safe_insert_into_table(township_insert, client, 'Township', township__json_file, row => [row.obshtina, row.name, row.name_en, row.obshtina.substring(0,3), row.ekatte]);
-
-    const cityhalls_insert = ' INSERT INTO cityhall(id, name, name_en, township_id) VALUES($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING *;';
-    await safe_insert_into_table(cityhalls_insert, client, 'Cityhalls', cityhalls__json_file, row => [row.kmetstvo, row.name, row.name_en, row.kmetstvo.substring(0,5)]);
-
-    const villages_insert = ' INSERT INTO villages(id, name, name_en, township_id, district_id) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING RETURNING *;';
-    await safe_insert_into_table(villages_insert, client, 'Villages', villages__json_file, safe_get_village_values);
-
-    ///SELECT
-    // const district_res = await client.query('SELECT * from district;');
-    // console.log(district_res.rows);
-} catch (err) {
-    console.log("Error in query: ", err);
-} finally {
-    await client.end()
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 }
 
+async function routing_dispatcher(req, res) {
+  switch (req.url) {
+    case "/": {
+      await main_page_controller(req, res);
+      break;
+    }
+    default: {
+      res.statusCode = 404;
+      res.end("Not found");
+    }
+  }
+}
+
+async function main_page_controller(req, res) {
+  try {
+    await bulk_inserts_from_json(client);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello, main page!\n");
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 500;
+    res.end("Server error");
+    await client.end();
+  }
+}
+
+//CHECK FOR CONNECTION
+// const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+// console.log(res.rows[0].message)
+
+// const curr_db = await client.query('SELECT current_database()');
+// console.log(curr_db.rows);
+
+///INSERT
+//await bulk_inserts_from_json(client);
+
+///SELECT
+// const district_res = await client.query('SELECT * from district;');
+// console.log(district_res.rows);
--- src/services/databese_inserts.js
+++ src/services/databese_inserts.js
@@ -1,13 +1,37 @@
-import { validateMany } from "../utils/validation.js";
-import dotenv from 'dotenv';
-import {withErrorHandling } from "../utils/errorHandling.js";
+import district__json_file from '../data/ek_obl.json' with {type: 'json'};
+import township__json_file from '../data/ek_obst.json' with {type: 'json'};
+import cityhalls__json_file from '../data/ek_kmet.json' with {type: 'json'};
+import villages__json_file from '../data/ek_sobr.json' with {type: 'json'};
 
-export { safe_insert_into_table};
+import dotenv from 'dotenv';
+import { validateMany } from "../utils/validation.js";
+import {withErrorHandling } from "../utils/errorHandling.js";
+import { safe_get_village_values } from '../services/village.js';
+
+export { bulk_inserts_from_json};
 
 dotenv.config();
 
 // const safe_insert_into_table = withErrorHandling(insert_into_table, {notRethrow: true});
 const safe_insert_into_table = withErrorHandling(insert_into_table);
+
+async function bulk_inserts_from_json(client) {
+  validateMany ( {
+    client: process.env.VALIDATION_TYPE_DB_CLIENT,
+  }, arguments);
+
+  const district_insert = 'INSERT INTO district(id, name, name_en, center_id) VALUES($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING *';
+  await safe_insert_into_table(district_insert, client, 'District', district__json_file, row => [row.oblast, row.name, row.name_en, row.ekatte]);
+  
+  const township_insert = ' INSERT INTO township(id, name, name_en, district_id, center_id) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING RETURNING *;';
+  await safe_insert_into_table(township_insert, client, 'Township', township__json_file, row => [row.obshtina, row.name, row.name_en, row.obshtina.substring(0,3), row.ekatte]);
+  
+  const cityhalls_insert = ' INSERT INTO cityhall(id, name, name_en, township_id) VALUES($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING *;';
+  await safe_insert_into_table(cityhalls_insert, client, 'Cityhalls', cityhalls__json_file, row => [row.kmetstvo, row.name, row.name_en, row.kmetstvo.substring(0,5)]);
+  
+  const villages_insert = ' INSERT INTO villages(id, name, name_en, township_id, district_id) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING RETURNING *;';
+  await safe_insert_into_table(villages_insert, client, 'Villages', villages__json_file, safe_get_village_values);
+}
 
 async function insert_into_table(insert_statement, db_client, table_name, file_json, valueMapper) {
   /////////////////////////////////////////////////////
